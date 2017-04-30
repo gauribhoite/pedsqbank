4f199d46c86c3ae4113ca19151582bd2bc87c210
Gauri Bhoite <gauri.bhoite@babylonhealth.com>
2017-05-01 00:06:07 +0100

    change to sqlite test database

--- tests/config.py
+++ tests/config.py
@@ -4,4 +4,4 @@ from config import BaseConfig
 class TestBaseConfig(BaseConfig):
     DEBUG = True
     SECRET_KEY ='NOTSECUREKEY'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@127.0.0.1/pedsqbank_test'
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
